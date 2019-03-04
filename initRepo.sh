caf41ab891cddbf1f89e6ddf5dca347de8b9ffdc
eli livshitz <eli.livshitz@gmail.com>
2019-03-04 08:54:23 +0200

    * Fixing initRepo to pull existing files beforehand

--- initRepo.sh
+++ initRepo.sh
@@ -3,9 +3,10 @@ repoUrl=$1
 echo "Initializing repository for existing and current folder at #$repoUrl!"
 
 git init
+git remote add origin $repoUrl
+git pull
 git add --all
 git commit -m 'Init'
-git remote add origin $repoUrl
 git remote -v
 git push origin master
 
