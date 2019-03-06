6a77f7ae15e663b076386d6e23fc9818cbe8b81b
Eli Livshitz <eli.livshitz@gmail.com>
2019-03-06 02:13:18 +0200

    + ls tree (lst)

---
+++ setups/lst.sh
@@ -0,0 +1,5 @@
+
+echo "lst()" >> ~/.bash_profile
+echo "{" >> ~/.bash_profile
+echo "	ls -R $1 | grep \":$\" | sed -e \'s\/:$\/\/\' -e \'s\/[^-][^\\\/]*\\\/\/--\/g\' -e \'s\/^\/   \/\' -e \'s\/-\/|\/\'" >> ~/.bash_profile
+echo "}" >> ~/.bash_profile
