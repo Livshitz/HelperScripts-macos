01fd4b057b0e42370468245cbc7150832524753b
Eli Livshitz <eli.livshitz@gmail.com>
2019-04-03 01:15:45 +0300

    * adjustments to mov2gif

--- mov2gif.sh
+++ mov2gif.sh
@@ -1,12 +1,25 @@
-#!/bin/bash
+.#!/bin/bash
 src=$1
 dest=$2
+scale=${3:-1}
 
-echo "options: ${@:3}"
-# ffmpeg -i $src -r 10 $dest
-ffmpeg -i $src  -vf "scale=iw/2:ih/2"  -b 100000 -r 10 $dest
-gifsicle -O5 $dest -o $dest --scale 0.8 ${@:3}
+if [ -z "$var" ]
+then
+	dest="${src%.*}.gif"
+fi
 
+echo "options: $3 - ${@:4}"
+rm $dest
+ffmpeg -i $src  -vf "scale=iw:ih" -pix_fmt rgb24 -b:v 100000 -r 10 $dest
+gifsicle -O5 $dest -o $dest --scale $scale
 
 echo "Done!"
 
+
+# Convert video to gif file.
+# Usage: video2gif video_file (scale) (fps)
+# video2gif() {
+#   ffmpeg -y -i "${1}" -vf fps=${3:-10},scale=${2:-320}:-1:flags=lanczos,palettegen "${1}.png"
+#   ffmpeg -i "${1}" -i "${1}.png" -filter_complex "fps=${3:-10},scale=${2:-320}:-1:flags=lanczos[x];[x][1:v]paletteuse" "${1}".gif
+#   rm "${1}.png"
+# }
