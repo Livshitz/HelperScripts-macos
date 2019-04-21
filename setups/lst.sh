02dee7785ae5b510c336fd85224db6928d544412
eli livshitz <eli.livshitz@gmail.com>
2019-04-21 19:23:05 +0300

    + restartCamera * fixing lst

--- setups/lst.sh
+++ setups/lst.sh
@@ -1,5 +1,7 @@
 
+echo "" >> ~/.bash_profile
+echo "# LST:" >> ~/.bash_profile
 echo "lst()" >> ~/.bash_profile
 echo "{" >> ~/.bash_profile
-echo "	ls -R $1 | grep \":$\" | sed -e \'s\/:$\/\/\' -e \'s\/[^-][^\\\/]*\\\/\/--\/g\' -e \'s\/^\/   \/\' -e \'s\/-\/|\/\'" >> ~/.bash_profile
+echo "	ls -R \$1 | grep \":$\" | sed -e  's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'" >> ~/.bash_profile
 echo "}" >> ~/.bash_profile
