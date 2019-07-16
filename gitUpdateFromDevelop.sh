f78a064cc15ff02f6fd004f9cc6120674d3c497a
eli livshitz <eli.livshitz@gmail.com>
2019-07-16 19:19:57 +0300

    * make `local` folder not record changes

---
+++ gitUpdateFromDevelop.sh
@@ -0,0 +1,8 @@
+curBranch=`git branch | grep \* | cut -d ' ' -f2`
+targetBranch=develop
+git pull
+git checkout $targetBranch
+git pull
+git checkout $curBranch 
+git merge $targetBranch -m "= merge from develop"
+git push
