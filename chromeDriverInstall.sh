fa69ea6f19a124f97eec26a285dbc52e17319363
eli livshitz <eli.livshitz@gmail.com>
2019-08-05 10:46:38 +0300

    + chromeDriverInstall

---
+++ chromeDriverInstall.sh
@@ -0,0 +1,15 @@
+set -e
+
+ARCH=${1:-"mac64"}
+CHROMEDRIVER_VERSION=${2:-"76.0.3809.25"}
+CACHED_DOWNLOAD="${HOME}/cache/chromedriver_$ARCH_${CHROMEDRIVER_VERSION}.zip"
+
+CHACHEDIR=`dirname $CACHED_DOWNLOAD`
+mkdir -p $CHACHEDIR
+
+wget --continue --output-document "${CACHED_DOWNLOAD}" "https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_$ARCH.zip"
+
+rm -rf "${HOME}/bin/chromedriver"
+unzip -o "${CACHED_DOWNLOAD}" -d "${HOME}/bin"
+
+rm -rf $CACHED_DOWNLOAD
