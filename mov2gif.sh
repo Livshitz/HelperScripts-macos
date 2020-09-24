a5b392b7a4a2ccd10ac5ded9f1a29e44c07cdb63
Livshitz <elya.livshitz@gmail.com>
2020-09-24 12:16:28 +0300

    * fix mov2gif

--- mov2gif.sh
+++ mov2gif.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 src=$1
-dest=$2
-scale=${3:-1}
+# dest=$1
+scale=${2:-1}
 
 if [ -z "$dest" ]
 then
 	dest="${src%.*}.gif"
 fi
 
-echo "options: $3 - ${@:4}"
+echo "options: $scale - ${@:3}"
 rm $dest
+# ffmpeg -i $src -filter_complex "[0:v] fps=5,scale=iw:ih,split [a][b];[a] palettegen=stats_mode=single [p];[b][p] paletteuse=new=1" -pix_fmt rgb24 -b:v 100000 -r 10 $dest
 ffmpeg -i $src -vf "scale=iw:ih" -pix_fmt rgb24 -b:v 100000 -r 10 $dest
+# ffmpeg -i $src -f gif -r 10 -filter_complex "[0:v] split [a][b];[a] palettegen [p];[b][p] paletteuse" $dest
 gifsicle -O5 $dest -o $dest --scale $scale
 
 echo "Done!"
