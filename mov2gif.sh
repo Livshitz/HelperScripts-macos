2a1fa16a85058b1f98054439f6298382457a146d
Elya Livshitz <elya.livshitz@gmail.com>
2023-08-28 22:27:44 +0300

    * mov2gif: support framerate param

--- mov2gif.sh
+++ mov2gif.sh
@@ -2,6 +2,7 @@
 src=$1
 # dest=$1
 scale=${2:-1}
+framerate=${3:-10}
 
 if [ -z "$dest" ]
 then
@@ -11,7 +12,7 @@ fi
 echo "options: $scale - ${@:3}"
 rm $dest
 # ffmpeg -i $src -filter_complex "[0:v] fps=5,scale=iw:ih,split [a][b];[a] palettegen=stats_mode=single [p];[b][p] paletteuse=new=1" -pix_fmt rgb24 -b:v 100000 -r 10 $dest
-ffmpeg -i $src -vf "scale=iw:ih" -pix_fmt rgb24 -b:v 100000 -r 10 $dest
+ffmpeg -i $src -vf "scale=iw:ih" -pix_fmt rgb24 -b:v 100000 -r $framerate $dest
 # ffmpeg -i $src -f gif -r 10 -filter_complex "[0:v] split [a][b];[a] palettegen [p];[b][p] paletteuse" $dest
 gifsicle -O5 $dest -o $dest --scale $scale
 
