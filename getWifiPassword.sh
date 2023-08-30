55c2fbc27fd3524e0a1260b968f3530f952400e4
Elya Livshitz <elya.livshitz@gmail.com>
2023-08-30 13:46:57 +0300

    + getWifiPassword

---
+++ getWifiPassword.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+ssid=`/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I  | awk -F' SSID: '  '/ SSID: / {print $2}'`
+
+security find-generic-password -wa $ssid
